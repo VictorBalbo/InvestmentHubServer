9707337f2a999f3ddc5a64a49f4d965aef0cadac
Victor Balbo <victorb@take.net>
2020-10-12 23:02:05 -0300

    Create BaseServer for background running worker

---
+++ src/InvestmentHub/InvestmentHub.ServerApplication/Services/BaseService.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace InvestmentHub.ServerApplication.Services
+{
+    public abstract class BaseService : IService
+    {
+        private readonly SemaphoreSlim _semaphore;
+
+        private CancellationTokenSource _cts;
+        public Task Execution { get; private set; }
+
+        protected BaseService()
+        {
+            _semaphore = new SemaphoreSlim(1);
+        }
+
+        protected abstract Task ExecuteAsync(CancellationToken cancellationToken);
+
+        public async Task StartAsync(CancellationToken cancellationToken)
+        {
+            await _semaphore.WaitAsync(cancellationToken).ConfigureAwait(false);
+            try
+            {
+                if (Execution != null)
+                {
+                    throw new InvalidOperationException($"The service '{GetType().Name}' is already started");
+                }
+
+                cancellationToken.ThrowIfCancellationRequested();
+
+                _cts = new CancellationTokenSource();
+                Execution = Task.Run(() =>
+                {
+                    ExecuteAsync(_cts.Token);
+                }, _cts.Token);
+            }
+            finally
+            {
+                _semaphore.Release();
+            }
+        }
+
+        public void Stop()
+        {
+            _semaphore.Wait();
+
+            try
+            {
+                if (Execution == null)
+                {
+                    throw new InvalidOperationException($"The service '{GetType().Name}' is already stoped");
+                }
+
+                _cts?.Cancel();
+                _cts?.Dispose();
+            }
+            finally
+            {
+                _semaphore.Release();
+            }
+        }
+    }
+}
---
+++ src/InvestmentHub/InvestmentHub.ServerApplication/Services/IService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace InvestmentHub.ServerApplication.Services
+{
+    /// <summary>
+    /// Defines a service basic operarions.
+    /// </summary>
+    public interface IService
+    {
+        /// <summary>
+        /// Gets the Task that represents the execution of the service.
+        /// </summary>
+        Task Execution { get; }
+
+        /// <summary>
+        /// Starts the execution of the service.
+        /// </summary>
+        /// <param name="cancellationToken">Token to allow the abortion of the service start</param>
+        /// <returns></returns>
+        Task StartAsync(CancellationToken cancellationToken);
+
+        /// <summary>
+        /// Signals to stop the execution of the service.
+        /// The loop is stopped when the Execution task is completed.
+        /// </summary>
+        void Stop();
+    }
+}
