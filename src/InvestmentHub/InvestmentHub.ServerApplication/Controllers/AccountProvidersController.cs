28f74923cd5b91a3cb083dfca8b130faaa6cb74d
Victor Balbo <victorb@take.net>
2021-02-02 19:28:51 -0300

    Lint suggestions

--- src/InvestmentHub/InvestmentHub.ServerApplication/Controllers/AccountProvidersController.cs
+++ src/InvestmentHub/InvestmentHub.ServerApplication/Controllers/AccountProvidersController.cs
@@ -24,25 +24,25 @@ namespace InvestmentHub.ServerApplication.Controllers
         [HttpGet]
         public async Task<IAsyncEnumerable<ProviderCredentials>> GetOwnProvidersCredentials(CancellationToken cancellationToken)
         {
-            return await _accountProvidersManager.GetSecuredAccountProviderCredentials(User.Identity.Name, cancellationToken);
+            return await _accountProvidersManager.GetSecuredAccountProviderCredentials(User.Identity?.Name, cancellationToken);
         }
 
         [HttpPut]
         [HttpPost]
         public async Task SetOwnProvidersCredentials([FromBody]ProviderCredentials providerCredentials, CancellationToken cancellationToken)
         {
-            if (providerCredentials.Email != User.Identity.Name)
+            if (providerCredentials.Email != User.Identity?.Name)
             {
                 throw new ArgumentException("Account email is not your own");
             }
 
-            await _accountProvidersManager.SetAccountProviderCredentials(User.Identity.Name, providerCredentials.Password, providerCredentials, cancellationToken);
+            await _accountProvidersManager.SetAccountProviderCredentials(User.Identity?.Name, providerCredentials.Password, providerCredentials, cancellationToken);
         }
 
         [HttpDelete]
         public async Task DeleteOwnProvidersCredentials([FromBody]ProviderCredentials providerCredentials, CancellationToken cancellationToken)
         {
-            var isDeleted = await _accountProvidersManager.DeleteAccountProviderCredentials(User.Identity.Name, providerCredentials, cancellationToken);
+            var isDeleted = await _accountProvidersManager.DeleteAccountProviderCredentials(User.Identity?.Name, providerCredentials, cancellationToken);
             if (!isDeleted)
             {
                 throw new ArgumentException("Could not delete the account provider");
